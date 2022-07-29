58b6eed2386bf2ed8ae2519b55d828f0d722d84f
Jeff Norton <jeff12ntn@gmail.com>
2022-07-29 16:47:57 -0600

    Initial commit

---
+++ Shared/micro_app_su_barcode_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  micro_app_su_barcode_scannerApp.swift
+//  Shared
+//
+//  Created by Jeff Norton on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct micro_app_su_barcode_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
